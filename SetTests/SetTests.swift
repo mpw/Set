baaafa864061d31e3fd0e2f7c55af54a24b3cda3
Marcel Weiher <marcel@metaobject.com>
2014-06-28 17:44:58 +0200

    updated tests

--- SetTests/SetTests.swift
+++ SetTests/SetTests.swift
@@ -5,12 +5,12 @@ import Set
 
 class SetTests: XCTestCase {
 	func testUnionAddsElementsFromBothOperands() {
-		XCTAssert(Set([1, 2, 3, 4]) + Set([3, 4, 5]) == Set([1, 2, 3, 4, 5]))
+		XCTAssert((Set([1, 2, 3, 4]) ∪ Set([3, 4, 5])) == Set([1, 2, 3, 4, 5]))
 	}
 	
 	func testUnionAssignmentModifiesInPlace() {
 		var c: Set<Int> = [1, 2, 3]
-		c += Set([3, 4, 5])
+		c ∪= Set([3, 4, 5])
 		
 		XCTAssert(c == Set([1, 2, 3, 4, 5]))
 	}
